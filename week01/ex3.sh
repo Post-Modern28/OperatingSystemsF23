9acaadd53e052692efcf9bb6e4ed153c5a058859
Mikhail Romanov <87484809+Post-Modern28@users.noreply.github.com>
2023-09-12 23:08:24 +0300

    Create ex3.sh

---
+++ week01/ex3.sh
@@ -0,0 +1,16 @@
+mkdir ~/week01/forRoot
+date
+sleep 3
+~/week01/forHome
+date
+sleep 3
+ls / > ~/week01/forRoot/root.txt
+date
+sleep 3
+ls ~ > ~/week01/forhome/home.txt
+echo "root.txt content:"
+cat ~/week01/forRoot/root.txt
+date
+sleep 3
+echo "home.txt content:"
+cat ~/week01/forhome/home.txt
