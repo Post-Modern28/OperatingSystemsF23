fa35689ebd3316bb0edf7ae3b37ccbb906aff819
Mikhail Romanov <87484809+Post-Modern28@users.noreply.github.com>
2023-10-17 19:07:09 +0300

    Add files via upload

--- week06/controller.c
+++ week06/controller.c
@@ -3,22 +3,35 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #define PID_FILE "/var/run/agent.pid"
 
 int main() {
-    FILE *fp = fopen(PID_FILE, "r");
-    if (fp == NULL) {
-        perror("Error opening PID file");
+    FILE *pid_file = fopen("/var/run/agent.pid", "r");
+    if (pid_file == NULL) {
+        printf("Error: No agent found.\n");
         exit(1);
     }
-    char pid_str[16];
-    fgets(pid_str, sizeof(pid_str), fp);
-    pid_t pid = atoi(pid_str);
-    fclose(fp);
+    int agent_pid;
+    fscanf(pid_file, "%d", &agent_pid);
+    fclose(pid_file);
+    printf("Agent found.\n");
 
-    kill(pid, SIGUSR1);
-    sleep(1);
-    kill(pid, SIGUSR2);
+    char command[10];
+    while (1) {
+        printf("Choose a command {\"read\", \"exit\", \"stop\", \"continue\"}: ");
+        scanf("%s", command);
+        if (strcmp(command, "read") == 0) {
+            kill(agent_pid, SIGUSR1);
+        } else if (strcmp(command, "exit") == 0) {
+            kill(agent_pid, SIGUSR2);
+            exit(0);
+        } else if (strcmp(command, "stop") == 0) {
+            kill(agent_pid, SIGSTOP);
+        } else if (strcmp(command, "continue") == 0) {
+            kill(agent_pid, SIGCONT);
+        }
+    }
 
     return 0;
 }
