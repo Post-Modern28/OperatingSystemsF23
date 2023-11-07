55c569be4e1e38e867fad3caf32e9ec19956fcaf
Mikhail Romanov <87484809+Post-Modern28@users.noreply.github.com>
2023-11-08 01:16:16 +0300

    many changes
    fixed size of page_table in truncate() function, replaced searching algorithm with random search. Now instead of 1 or 0 field .referenced contains pid of mmu, and instead of sleeping for 1 second mmu waits for signal back from pager.

--- week08/pager.c
+++ week08/pager.c
@@ -24,9 +24,10 @@ void update_page_on_disk(int page_num){
 } 
 
 void load_on_RAM(int cell,int page_num){
-	page_table[page_num].referenced = 0;
 	page_table[page_num].frame = cell;
 	page_table[page_num].valid = true;
+	kill(page_table[page_num].referenced, SIGUSR1);
+	page_table[page_num].referenced = 0;
 }
  
 void swap_from_RAM(int cell, int page_num){
@@ -37,21 +38,28 @@ void swap_from_RAM(int cell, int page_num){
     	page_table[page_num].dirty = false;
     } 
 }
+
+int random_pick(PTE* page_table){
+		int rand_idx = rand() % FRAME_SIZE;
+		for (int i = 0; i < PAGE_SIZE; i++){
+			if (page_table[i].frame == rand_idx){
+				return i;
+			}
+				
+		}
+	}
 void sigusr1_handler(int signo) {
     for (int i = 0; i < PAGE_SIZE; i++){
     	if (page_table[i].referenced && !page_table[i].valid){
     		if (RAM_load < FRAME_SIZE){
     			load_on_RAM(RAM_load, i);
-    			RAM_load++;
+    			++RAM_load;
     		}
     		else{
-    			for (int j = 0; j < PAGE_SIZE; j++){
-    				if (page_table[j].valid){
-    					int cell = page_table[j].frame;
-    					swap_from_RAM(cell, j);
-    					load_on_RAM(cell, i);
-    				}
-    			}
+    			int j = random_pick(page_table);
+    			int cell = page_table[j].frame;
+    			swap_from_RAM(cell, j);
+    			load_on_RAM(cell, i);	
     		}
     	break;
     	}
@@ -78,7 +86,7 @@ int main(int argc, char *argv[]) {
     
     mkdir("/tmp/ex2", 0666);
     int fd = open("/tmp/ex2/pagetable", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    ftruncate(fd, PAGE_SIZE);
+    ftruncate(fd, sizeof(PTE)*PAGE_SIZE);
     page_table = (PTE*) mmap(NULL, sizeof(PTE) * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     
     signal(SIGUSR1, sigusr1_handler);
@@ -89,7 +97,7 @@ int main(int argc, char *argv[]) {
         page_table[i].dirty = false;
         page_table[i].referenced = 0;
     }
-    //printf("Pager pid is %d\n", getpid());
+    printf("Pager pid is %d\n", getpid());
     
     while (1){
     	pause();
